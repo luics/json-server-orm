dd0463e916b059e49b42576496ee21b07ff86ed9
luics <luics.xu@gmail.com>
2021-07-14 13:57:47 +0800

    export Validation

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,6 @@ export type OperatorNameValue = { name: string, value: number };
 export type NameValue = { name: string, value: any };
 export type JSONType = 'string' | 'number' | 'integer' | 'boolean' | 'object' | 'array' | 'null';
 export class ValidationError extends Error {
-  // eslint-disable-next-line no-unused-vars
   constructor(message: string, public field: string) {
     super(message);
   }
